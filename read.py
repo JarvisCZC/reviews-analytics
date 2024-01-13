f43bd98cf9718abe1b6934de6c2d7c36e5fcab17
JarvisCZC <qazwsx8912@gmail.com>
2024-01-13 22:04:43 +0800

    add filter function

--- read.py
+++ read.py
@@ -13,3 +13,11 @@ for d in data:
 	sum_len += len(d) # sum_len = sum_len + len(d)
 	print(sum_len)
 print('每一筆留言的平均長度為', sum_len / len(data))
+
+#篩選功能
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
